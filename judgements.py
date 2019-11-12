b77d96edb76578eb08fdc5d98398775b2773a25f
saxAllan <asahi_shimizu@outlook.com>
2019-11-12 15:37:19 +0900

    update judgement

--- judgements.py
+++ judgements.py
@@ -1,20 +1,32 @@
 import input
 
 #----------labelling start----------
-def labelling(x, y, labelno):
+def labelling_x(x, y, labelno, list_equal):
     #xについて
     tmpx = x
-    tmpx = y
+    tmpy = y
+    cnt=0
     sa = (input.data[tmpx][tmpy][0] - input.data[tmpx + 1][tmpy][0]) * (input.data[tmpx][tmpy][0] - input.data[tmpx + 1][tmpy][0])
-    while sa<0.25:
+    while sa < 0.25:
+        if input.data[tmpx + 1][tmpy][1] != 0:
+            #input.data[tmpx + 1][tmpy][1]とlabelnoのデータを二次元配列に格納
+            list_equal[0].append(input.data[tmpx + 1][tmpy][1])
+            list_equal[1].append(labelno)
         input.data[tmpx + 1][tmpy][1] = labelno
         tmpx += 1
         sa = (input.data[tmpx][tmpy][0] - input.data[tmpx + 1][tmpy][0]) * (input.data[tmpx][tmpy][0] - input.data[tmpx + 1][tmpy][0])
+
+def labelling_y(x, y, labelno, list_equal):
     #yについて
     tmpx = x
     tmpx = y
     sa = (input.data[tmpx][tmpy][0] - input.data[tmpx][tmpy + 1][0]) * (input.data[tmpx][tmpy][0] - input.data[tmpx][tmpy + 1][0])
     while sa < 0.25:
+        if input.data[tmpx + 1][tmpy][1] != 0:
+            #input.data[tmpx][tmpy + 1][1]とlabelnoのデータを二次元配列に格納
+            list_equal[0].append(input.data[tmpx][tmpy + 1][1])
+            list_equal[1].append(labelno)
+
         input.data[tmpx][tmpy + 1][1] = labelno
         tmpy += 1
         sa = (input.data[tmpx][tmpy][0] - input.data[tmpx][tmpy + 1][0]) * (input.data[tmpx][tmpy][0] - input.data[tmpx][tmpy + 1][0])
@@ -26,11 +38,15 @@ start_y = 0
 pt_x = 0
 pt_y = 0
 label = 1
-chkcount = 0
+equal = []
+for i in range (2):
+    equal.append([])
 
 input.data[pt_x][pt_y][1] = label
 
-while chkcount < input.count_x * input.count_y:
-    labelling(pt_x, pt_y, label)
-    
+
+for i in range(input.count_y):
+
+
+labelling_x(pt_x, pt_y, label,)
 #----------main end----------
