0d6d85d2fdd76722405568756de957961114d284
saxAllan <asahi_shimizu@outlook.com>
2019-11-15 12:40:04 +0900

    update judgements

--- judgements.py
+++ judgements.py
@@ -14,45 +14,31 @@ def labelling_x(x, y, labelno, list_equal):
         sa = (input.data[x][y][0] - input.data[x + 1][y][0]) * (input.data[x][y][0] - input.data[x + 1][y][0])
     print("labelling_x", x, y, labelno)
 
-'''多分いらない
-def labelling_y(x, y, labelno, list_equal):
-    #yについて
-    sa = (input.data[x][y][0] - input.data[x][y + 1][0]) * (input.data[x][y][0] - input.data[x][y + 1][0])
-    while sa < 0.25:
-        if input.data[x + 1][y][1] != 0:
-            #input.data[x][y + 1][1]とlabelnoのデータを二次元配列に格納
-            list_equal[0].append(input.data[x][y + 1][1])
-            list_equal[1].append(labelno)
-        input.data[x][y + 1][1] = labelno
-        y += 1
-        sa = (input.data[x][y][0] - input.data[x][y + 1][0]) * (input.data[x][y][0] - input.data[x][y + 1][0])
-'''
-
 def labelling_base(base_x, base_y, base_label, base_equal):
     for i in range(base_x, input.count_x):
         print(input.data[base_x][base_y][1])
         if (input.data[base_x][base_y][1] == 0):
             print("入った")
             labelling_x(base_x, base_y, base_label, base_equal)
-            if (input.data[base_x][base_y + i][0] - input.data[base_x][base_y + i + 1][0]) * (input.data[base_x][base_y + i][0] - input.data[base_x][base_y + i + 1][0]) < 0.25:
+            if base_x == input.count_x - 3 or base_y == input.count_y - 3:
+                break
+            elif (input.data[base_x][base_y + i][0] - input.data[base_x][base_y + i + 1][0]) * (input.data[base_x][base_y + i][0] - input.data[base_x][base_y + i + 1][0]) < 0.25:
                 labelling_base(base_x, base_y + 1, base_label, base_equal)
-        else:
-            print("入ってない")
-                
-
 
 #----------labelling end----------
 
 #----------main start----------
+#初期化
+equal = []
+for i in range (2):
+    equal.append([])
 start_x = 0
 start_y = 0
 pt_x = 0
 pt_y = 0
 label = 1
-equal = []
-for i in range (2):
-    equal.append([])
 
+#ラベリング
 #input.data[pt_x][pt_y][1] = label
 for i in range(input.count_x):
     for j in range(input.count_y):
