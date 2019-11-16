09e7b94ab4b2175954c5d2a215d0e9c68a3ffe1d
saxAllan <asahi_shimizu@outlook.com>
2019-11-16 21:58:01 +0900

    update judgements

--- judgements.py
+++ judgements.py
@@ -5,22 +5,24 @@ def labelling_x(x, y, labelno, list_equal):
     #xについて
     sa = (input.data[x][y][0] - input.data[x + 1][y][0]) * (input.data[x][y][0] - input.data[x + 1][y][0])
     while sa < 0.25:
-        if input.data[x + 1][y][1] != 0:
-            #input.data[x + 1][y][1]とlabelnoのデータを二次元配列に格納
+        input.data[x + 1][y][1] = labelno
+        if input.data[x + 1][y][1] != 0: #既にラベリングされてたら、input.data[x + 1][y][1]とlabelnoのデータを二次元配列に格納
             list_equal[0].append(input.data[x + 1][y][1])
             list_equal[1].append(labelno)
-        input.data[x + 1][y][1] = labelno
-        x += 1
+        x += 1 #次のxへ
         sa = (input.data[x][y][0] - input.data[x + 1][y][0]) * (input.data[x][y][0] - input.data[x + 1][y][0])
-    print("labelling_x", x, y, labelno)
+    print("labelling_x", x, y, labelno) #デバッグ
 
 def labelling_base(base_x, base_y, base_label, base_equal):
+    #横方向へ
     for i in range(base_x, input.count_x):
-        print(input.data[base_x][base_y][1])
-        if (input.data[base_x][base_y][1] == 0):
+        #print(input.data[base_x][base_y][1])
+        if (input.data[i][base_y][1] == 0):
             print("入った")
-            labelling_x(base_x, base_y, base_label, base_equal)
-            if base_x == input.count_x - 3 or base_y == input.count_y - 3:
+            labelling_x(i, base_y, base_label, base_equal)
+    #上方向へ
+    for i in range(base_x, input.count_x):
+        if base_x == input.count_x - 3 or base_y == input.count_y - 3:
                 break
             elif (input.data[base_x][base_y + i][0] - input.data[base_x][base_y + i + 1][0]) * (input.data[base_x][base_y + i][0] - input.data[base_x][base_y + i + 1][0]) < 0.25:
                 labelling_base(base_x, base_y + 1, base_label, base_equal)
