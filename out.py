120eedc8ea9cd4b25729750fd34d16bc2e5e5fc7
saxAllan <asahi_shimizu@outlook.com>
2019-11-24 18:38:33 +0900

    update out

--- out.py
+++ out.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 filename_i = input("入力ファイル名（拡張子は不要):")
-filename_o = "output"
 
 print(filename_i + ".datを読み込んでいます")
 f = open(filename_i + ".dat", "r")
@@ -46,36 +45,37 @@ print(len(pts))
 print(len(pts[tri.simplices]))
 
 #出力
-filename_o = open('output.wrl', 'w')
+filename_o = "output"
+outfile = open(filename_o + '.wrl', 'w')
 start = '#VRML V2.0 utf8\n'
-filename_o.write(start)
+outfile.write(start)
 text1 = 'Shape {\ngeometry IndexedFaceSet { \nsolid FALSE\ncoord Coordinate { \npoint [\n'
-filename_o.write(text1)
+outfile.write(text1)
 
 for i in range(len(pts)):
     if i % 10000 == 0:
         print("point ", i)
     for j in range(3):
-        filename_o.writelines(str(pts[i][j]))
+        outfile.writelines(str(pts[i][j]))
         if j == 0 or j == 1:
             space = ' '
-            filename_o.write(space)
+            outfile.write(space)
     conma = ',\n'
-    filename_o.write(conma)
+    outfile.write(conma)
 
 textc = ']\n}\ncoordIndex [\n'
-filename_o.write(textc)
+outfile.write(textc)
 
 for i in range(len(pts[tri.simplices])):
     if i % 10000 == 0:
         print("coordIndex ", i)
     for j in range(3):
-        filename_o.writelines(str(tri.simplices[i][j]))
+        outfile.writelines(str(tri.simplices[i][j]))
         conma2 = ','
-        filename_o.write(conma2)
+        outfile.write(conma2)
     conma3 = '-1,\n'
-    filename_o.write(conma3)
+    outfile.write(conma3)
 
 text2 = ']\n}\nappearance Appearance { \nmaterial\nMaterial {}\n}\n}\n'
-filename_o.write(text2)
-filename_o.close()
+outfile.write(text2)
+outfile.close()
