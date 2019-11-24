40f9e6af45d4ef8bdffc349c7c84d6a6ad536f65
saxAllan <asahi_shimizu@outlook.com>
2019-11-24 21:27:43 +0900

    update out

--- OLD_out.py
+++ OLD_out.py
@@ -1,47 +1,60 @@
+# out Ver.1.2(20191124-2)
+
 from scipy.spatial import Delaunay
 import matplotlib.pyplot as plt
 import numpy as np
-import input
-print("\n========================================")
-print("  out Ver. 1.3 (20191124-7)")
-print("========================================\n")
 
+filename_i = input("入力ファイル名（拡張子は不要):")
 
+print(filename_i + ".datを読み込んでいます")
+f = open(filename_i + ".dat", "r")
+lines = f.readlines()
+orgdata = []
+for i in lines:
+    orgdata.append(list(map(float, i.split())))
+f.close()
+"""
+for i in range(len(orgdata)):
+    print(orgdata[i])
+print(" ...",len(orgdata),"個のデータを読み込みました")
+"""
 newdata = []
 zdata = []
-for i in range(input.count_y):
-    for j in range(input.count_x):
-        zdata.append(input.data[j][i][0])
-        newdata.append(j)
-        newdata.append(i)
 
-print("Delaunay 処理中(多少時間がかかります)")
+for i in range(len(orgdata)):
+    zdata.append(orgdata[i][2])
+    for j in range(2):
+        newdata.append(orgdata[i][j])
+
 pts = np.array(newdata).reshape(-1, 2)
 ztmp = np.array(zdata)
+
 tri = Delaunay(pts)
+
 newdata = pts.tolist()
 
-temp_len = len(newdata)
-for i in range(temp_len):
+for i in range(len(newdata)):
     newdata[i].append(zdata[i])
+
 pts = np.array(newdata)
+
 fig = plt.figure()
-print("Delaunay 処理完了   (len(pts)=", len(pts), "), (len(pts[tri.simplices])=", len(pts[tri.simplices]), ")")
+
+print("ドロネー完了")
+print(len(pts))
+print(len(pts[tri.simplices]))
 
 #出力
 filename_o = "output"
 outfile = open(filename_o + '.wrl', 'w')
-print(filename_o + ".wrl として出力中...")
-
 start = '#VRML V2.0 utf8\n'
 outfile.write(start)
 text1 = 'Shape {\ngeometry IndexedFaceSet { \nsolid FALSE\ncoord Coordinate { \npoint [\n'
 outfile.write(text1)
 
-temp_len=len(pts)
-for i in range(temp_len):
+for i in range(len(pts)):
     if i % 10000 == 0:
-        print("\rpoint ", i // 10000, "/", temp_len // 10000, end="")
+        print("point ", i)
     for j in range(3):
         outfile.writelines(str(pts[i][j]))
         if j == 0 or j == 1:
@@ -49,23 +62,20 @@ for i in range(temp_len):
             outfile.write(space)
     conma = ',\n'
     outfile.write(conma)
-print("   処理完了")
 
 textc = ']\n}\ncoordIndex [\n'
 outfile.write(textc)
-temp_len=len(pts[tri.simplices])
-for i in range(temp_len):
+
+for i in range(len(pts[tri.simplices])):
     if i % 10000 == 0:
-        print("\rcoordIndex ", i // 10000, "/", temp_len // 10000, end="")
+        print("coordIndex ", i)
     for j in range(3):
         outfile.writelines(str(tri.simplices[i][j]))
         conma2 = ','
         outfile.write(conma2)
     conma3 = '-1,\n'
     outfile.write(conma3)
-print("処理完了")
+
 text2 = ']\n}\nappearance Appearance { \nmaterial\nMaterial {}\n}\n}\n'
 outfile.write(text2)
 outfile.close()
-
-print("\n出力完了 ("+filename_o + ".wrl )\n")
--- out.py
+++ out.py
@@ -1,60 +1,47 @@
-# out Ver.1.2(20191124-2)
-
 from scipy.spatial import Delaunay
 import matplotlib.pyplot as plt
 import numpy as np
+import input
+print("\n========================================")
+print("  out Ver. 1.3 (20191124-7)")
+print("========================================\n")
 
-filename_i = input("入力ファイル名（拡張子は不要):")
 
-print(filename_i + ".datを読み込んでいます")
-f = open(filename_i + ".dat", "r")
-lines = f.readlines()
-orgdata = []
-for i in lines:
-    orgdata.append(list(map(float, i.split())))
-f.close()
-"""
-for i in range(len(orgdata)):
-    print(orgdata[i])
-print(" ...",len(orgdata),"個のデータを読み込みました")
-"""
 newdata = []
 zdata = []
+for i in range(input.count_y):
+    for j in range(input.count_x):
+        zdata.append(input.data[j][i][0])
+        newdata.append(j)
+        newdata.append(i)
 
-for i in range(len(orgdata)):
-    zdata.append(orgdata[i][2])
-    for j in range(2):
-        newdata.append(orgdata[i][j])
-
+print("Delaunay 処理中(多少時間がかかります)")
 pts = np.array(newdata).reshape(-1, 2)
 ztmp = np.array(zdata)
-
 tri = Delaunay(pts)
-
 newdata = pts.tolist()
 
-for i in range(len(newdata)):
+temp_len = len(newdata)
+for i in range(temp_len):
     newdata[i].append(zdata[i])
-
 pts = np.array(newdata)
-
 fig = plt.figure()
-
-print("ドロネー完了")
-print(len(pts))
-print(len(pts[tri.simplices]))
+print("Delaunay 処理完了   (len(pts)=", len(pts), "), (len(pts[tri.simplices])=", len(pts[tri.simplices]), ")")
 
 #出力
 filename_o = "output"
 outfile = open(filename_o + '.wrl', 'w')
+print(filename_o + ".wrl として出力中...")
+
 start = '#VRML V2.0 utf8\n'
 outfile.write(start)
 text1 = 'Shape {\ngeometry IndexedFaceSet { \nsolid FALSE\ncoord Coordinate { \npoint [\n'
 outfile.write(text1)
 
-for i in range(len(pts)):
+temp_len=len(pts)
+for i in range(temp_len):
     if i % 10000 == 0:
-        print("point ", i)
+        print("\rpoint ", i // 10000, "/", temp_len // 10000, end="")
     for j in range(3):
         outfile.writelines(str(pts[i][j]))
         if j == 0 or j == 1:
@@ -62,20 +49,23 @@ for i in range(len(pts)):
             outfile.write(space)
     conma = ',\n'
     outfile.write(conma)
+print("   処理完了")
 
 textc = ']\n}\ncoordIndex [\n'
 outfile.write(textc)
-
-for i in range(len(pts[tri.simplices])):
+temp_len=len(pts[tri.simplices])
+for i in range(temp_len):
     if i % 10000 == 0:
-        print("coordIndex ", i)
+        print("\rcoordIndex ", i // 10000, "/", temp_len // 10000, end="")
     for j in range(3):
         outfile.writelines(str(tri.simplices[i][j]))
         conma2 = ','
         outfile.write(conma2)
     conma3 = '-1,\n'
     outfile.write(conma3)
-
+print("処理完了")
 text2 = ']\n}\nappearance Appearance { \nmaterial\nMaterial {}\n}\n}\n'
 outfile.write(text2)
 outfile.close()
+
+print("\n出力完了 ("+filename_o + ".wrl )\n")
