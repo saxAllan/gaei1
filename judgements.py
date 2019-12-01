c91759135f6e516d0e77e91c5439d5da27f05694
saxAllan <asahi_shimizu@outlook.com>
2019-12-02 01:55:02 +0900

    update judge

--- judgements.py
+++ judgements.py
@@ -1,12 +1,13 @@
+import collections
+import statistics
+import numpy
+import input
 print("\n========================================")
 print("  judgements Ver. 1.26 (20191126)")
 print("========================================\n")
-import input
-import numpy
-import statistics
 
 
-#初期化
+# 初期化
 start_x = 0
 start_y = 0
 label = 0
@@ -14,35 +15,54 @@ max_label = 0
 
 print("judgements 処理中...")
 
-#ラベリング
+th = 1  # しきい値
+
+# ラベリング
 for i in range(input.count_x):
     for j in range(input.count_y):
+        # 自分自身の判定
         if input.data[i][j][1] != 0:
             label = input.data[i][j][1]
         else:
             label = max_label + 1
-            max_label += 1        
-        print("\r", i * input.count_y + j + 1, "/", input.count_x * input.count_y, end="   ")
+            max_label += 1
+
+        print("\r", i * input.count_y + j + 1, "/",
+              input.count_x * input.count_y, end="   ")
         input.data[i][j][1] = label
-        if j < input.count_y - 2:
-            if (input.data[i][j + 1][0] - input.data[i][j][0]) * (input.data[i][j + 1][0] - input.data[i][j][0]) < 4:
+        # 上方向
+        if j < input.count_y - 3:
+            if (input.data[i][j + 2][0] - input.data[i][j][0]) * (input.data[i][j + 2][0] - input.data[i][j][0]) < th:
+                input.data[i][j + 2][1] = label
                 input.data[i][j + 1][1] = label
-        if i < input.count_x - 2:
-            if (input.data[i + 1][j][0] - input.data[i][j][0]) * (input.data[i + 1][j][0] - input.data[i][j][0]) < 4:
+            elif (input.data[i][j + 1][0] - input.data[i][j][0]) * (input.data[i][j + 1][0] - input.data[i][j][0]) < th:
+                input.data[i][j + 1][1] = label
+        elif j < input.count_y - 2:
+            if (input.data[i][j + 1][0] - input.data[i][j][0]) * (input.data[i][j + 1][0] - input.data[i][j][0]) < th:
+                input.data[i][j + 1][1] = label
+        # 右方向
+        if i < input.count_x - 3:
+            if (input.data[i + 2][j][0] - input.data[i][j][0]) * (input.data[i + 2][j][0] - input.data[i][j][0]) < th:
+                input.data[i + 2][j][1] = label
+                input.data[i + 1][j][1] = label            
+            elif (input.data[i + 1][j][0] - input.data[i][j][0]) * (input.data[i + 1][j][0] - input.data[i][j][0]) < th:
                 input.data[i + 1][j][1] = label
+        elif i < input.count_x - 2:
+            if (input.data[i + 1][j][0] - input.data[i][j][0]) * (input.data[i + 1][j][0] - input.data[i][j][0]) < th:
+                input.data[i + 1][j][1] = label
+        # 右上方向
         if i < input.count_x - 2 and j < input.count_y - 2:
-            if (input.data[i + 1][j + 1][0] - input.data[i][j][0]) * (input.data[i + 1][j + 1][0] - input.data[i][j][0]) < 4:
+            if (input.data[i + 1][j + 1][0] - input.data[i][j][0]) * (input.data[i + 1][j + 1][0] - input.data[i][j][0]) < th:
                 input.data[i + 1][j + 1][1] = label
 print("処理終了")
 
-#デバッグ
+# デバッグ
 test = []
 for i in range(input.count_x):
     for j in range(input.count_y):
         test.append(input.data[i][j][1])
 test.sort()
 #mode = statistics.mode(test)
-import collections
 mode_org = collections.Counter(test).most_common()
 nokori = []
 for i in range(input.count_x):
@@ -53,7 +73,7 @@ for i in range(input.count_y):
     for j in range(input.count_x):
         nokori[j].append([])
         nokori[j][i].append(0)
-for k in range(150):
+for k in range(10):
     mode = mode_org[k][0]
     print(mode)
     for i in range(input.count_y):
@@ -63,9 +83,9 @@ for k in range(150):
                 nokori[j][i][0] = input.data[j][i][0]
 
 print(count_in)
-#print(nokori)
+# print(nokori)
 
 #test_np = numpy.array(test)
 
 
-#print(test)
+# print(test)
