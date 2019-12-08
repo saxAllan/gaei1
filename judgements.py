404025643127718ebe2f26daba6a7925d3bae983
saxAllan <asahi_shimizu@outlook.com>
2019-12-08 23:31:19 +0900

    datinput -> norminput

--- judgements.py
+++ judgements.py
@@ -1,9 +1,10 @@
 import collections
 import statistics
 import numpy
-import datinput
+import norminput
+
 print("\n========================================")
-print("  judgements Ver. 2.31 (20191203)")
+print("  judgements Ver. 2.32 (20191208)")
 print("========================================\n")
 
 
@@ -20,48 +21,48 @@ print("judgements 処理中...")
 th = 1  # しきい値
 
 # ラベリング
-for i in range(datinput.count_x):
-    for j in range(datinput.count_y):
+for i in range(norminput.count_x):
+    for j in range(norminput.count_y):
         # 自分自身の判定
-        if datinput.data[i][j][1] != 0:
-            label = datinput.data[i][j][1]
+        if norminput.data[i][j][1] != 0:
+            label = norminput.data[i][j][1]
         else:
             label = max_label + 1
             max_label += 1
 
-        print("\r", i * datinput.count_y + j + 1, "/", datinput.count_x * datinput.count_y, end="   ")
-        datinput.data[i][j][1] = label
+        print("\r", i * norminput.count_y + j + 1, "/", norminput.count_x * norminput.count_y, end="   ")
+        norminput.data[i][j][1] = label
         # 上方向
-        if j < datinput.count_y - 2:
-            if (datinput.data[i][j + 1][0] - datinput.data[i][j][0]) * (datinput.data[i][j + 1][0] - datinput.data[i][j][0]) < th:
-                if datinput.data[i][j + 1][1] != 0:
+        if j < norminput.count_y - 2:
+            if (norminput.data[i][j + 1][0] - norminput.data[i][j][0]) * (norminput.data[i][j + 1][0] - norminput.data[i][j][0]) < th:
+                if norminput.data[i][j + 1][1] != 0:
                     samevalue[0].append(label)
-                    samevalue[1].append(datinput.data[i][j + 1][1])
-                datinput.data[i][j + 1][1] = label
+                    samevalue[1].append(norminput.data[i][j + 1][1])
+                norminput.data[i][j + 1][1] = label
         # 右方向
-        if i < datinput.count_x - 2:
-            if (datinput.data[i + 1][j][0] - datinput.data[i][j][0]) * (datinput.data[i + 1][j][0] - datinput.data[i][j][0]) < th:
-                if datinput.data[i + 1][j][1] != 0:
+        if i < norminput.count_x - 2:
+            if (norminput.data[i + 1][j][0] - norminput.data[i][j][0]) * (norminput.data[i + 1][j][0] - norminput.data[i][j][0]) < th:
+                if norminput.data[i + 1][j][1] != 0:
                     samevalue[0].append(label)
-                    samevalue[1].append(datinput.data[i + 1][j][1])
-                datinput.data[i + 1][j][1] = label
+                    samevalue[1].append(norminput.data[i + 1][j][1])
+                norminput.data[i + 1][j][1] = label
         # 右上方向
-        if i < datinput.count_x - 2 and j < datinput.count_y - 2:
-            if (datinput.data[i + 1][j + 1][0] - datinput.data[i][j][0]) * (datinput.data[i + 1][j + 1][0] - datinput.data[i][j][0]) < th:
-                if datinput.data[i][j + 1][1] != 0:
+        if i < norminput.count_x - 2 and j < norminput.count_y - 2:
+            if (norminput.data[i + 1][j + 1][0] - norminput.data[i][j][0]) * (norminput.data[i + 1][j + 1][0] - norminput.data[i][j][0]) < th:
+                if norminput.data[i][j + 1][1] != 0:
                     samevalue[0].append(label)
-                    samevalue[1].append(datinput.data[i + 1][j + 1][1])
-                datinput.data[i + 1][j + 1][1] = label
+                    samevalue[1].append(norminput.data[i + 1][j + 1][1])
+                norminput.data[i + 1][j + 1][1] = label
 print("処理終了")
 print(len(samevalue[0]), len(samevalue[1]))
 len_same = len(samevalue[0])
 '''
-for i in range(datinput.count_x):
-    for j in range(datinput.count_y):
+for i in range(norminput.count_x):
+    for j in range(norminput.count_y):
         for ii in range(len(samevalue[0])):
-            if datinput.data[i][j][1] == samevalue[1][ii]:
-                datinput.data[i][j][1] = samevalue[0][ii]
-            print("\r", i * datinput.count_y + j + 1, "/", datinput.count_x * datinput.count_y, end="   ")
+            if norminput.data[i][j][1] == samevalue[1][ii]:
+                norminput.data[i][j][1] = samevalue[0][ii]
+            print("\r", i * norminput.count_y + j + 1, "/", norminput.count_x * norminput.count_y, end="   ")
 '''
 newdata = [[[], []], [[], []]]
 for i in range(len_same):
@@ -71,61 +72,61 @@ for i in range(len_same):
 
 # デバッグ
 test = []
-for i in range(datinput.count_x):
-    for j in range(datinput.count_y):
-        test.append(datinput.data[i][j][1])
+for i in range(norminput.count_x):
+    for j in range(norminput.count_y):
+        test.append(norminput.data[i][j][1])
 test.sort()
 #mode = statistics.mode(test)
 mode_org = collections.Counter(test).most_common()
 nokori = []
-for i in range(datinput.count_x):
+for i in range(norminput.count_x):
     nokori.append([])
 count_in = 0
 
-for i in range(datinput.count_y):
-    for j in range(datinput.count_x):
+for i in range(norminput.count_y):
+    for j in range(norminput.count_x):
         nokori[j].append([])
         nokori[j][i].append(0)
 for k in range(100):
     mode = mode_org[k][0]
     print(mode)
-    for i in range(datinput.count_y):
-        for j in range(datinput.count_x):
-            if datinput.data[j][i][1] == mode:
+    for i in range(norminput.count_y):
+        for j in range(norminput.count_x):
+            if norminput.data[j][i][1] == mode:
                 count_in += 1
-                nokori[j][i][0] = datinput.data[j][i][0]
+                nokori[j][i][0] = norminput.data[j][i][0]
 
 
 
 '''
         # 上方向
-        if j < datinput.count_y - 2:
-            if (datinput.data[i][j + 1][0] - datinput.data[i][j][0]) * (datinput.data[i][j + 1][0] - datinput.data[i][j][0]) < th:
-                if datinput.data[i][j + 1][1] == 0:
-                    datinput.data[i][j + 1][1] = label
+        if j < norminput.count_y - 2:
+            if (norminput.data[i][j + 1][0] - norminput.data[i][j][0]) * (norminput.data[i][j + 1][0] - norminput.data[i][j][0]) < th:
+                if norminput.data[i][j + 1][1] == 0:
+                    norminput.data[i][j + 1][1] = label
                 else:
-                    for ii in range(datinput.count_x):
-                        for jj in range(datinput.count_y):
-                            if datinput.data[ii][jj][1] == datinput.data[i][j + 1][1]:
-                                datinput.data[ii][jj][1] = label
+                    for ii in range(norminput.count_x):
+                        for jj in range(norminput.count_y):
+                            if norminput.data[ii][jj][1] == norminput.data[i][j + 1][1]:
+                                norminput.data[ii][jj][1] = label
         # 右方向
-        if i < datinput.count_x - 2:
-            if (datinput.data[i + 1][j][0] - datinput.data[i][j][0]) * (datinput.data[i + 1][j][0] - datinput.data[i][j][0]) < th:
-                if datinput.data[i + 1][j][1] == 0:
-                    datinput.data[i + 1][j][1] = label
+        if i < norminput.count_x - 2:
+            if (norminput.data[i + 1][j][0] - norminput.data[i][j][0]) * (norminput.data[i + 1][j][0] - norminput.data[i][j][0]) < th:
+                if norminput.data[i + 1][j][1] == 0:
+                    norminput.data[i + 1][j][1] = label
                 else:
-                    for ii in range(datinput.count_x):
-                        for jj in range(datinput.count_y):
-                            if datinput.data[ii][jj][1] == datinput.data[i + 1][j][1]:
-                                datinput.data[ii][jj][1] = label
+                    for ii in range(norminput.count_x):
+                        for jj in range(norminput.count_y):
+                            if norminput.data[ii][jj][1] == norminput.data[i + 1][j][1]:
+                                norminput.data[ii][jj][1] = label
         # 右上方向
-        if i < datinput.count_x - 2 and j < datinput.count_y - 2:
-            if (datinput.data[i + 1][j + 1][0] - datinput.data[i][j][0]) * (datinput.data[i + 1][j + 1][0] - datinput.data[i][j][0]) < th:
-                if datinput.data[i + 1][j + 1][1] == 0:
-                    datinput.data[i + 1][j + 1][1] = label
+        if i < norminput.count_x - 2 and j < norminput.count_y - 2:
+            if (norminput.data[i + 1][j + 1][0] - norminput.data[i][j][0]) * (norminput.data[i + 1][j + 1][0] - norminput.data[i][j][0]) < th:
+                if norminput.data[i + 1][j + 1][1] == 0:
+                    norminput.data[i + 1][j + 1][1] = label
                 else:
-                    for ii in range(datinput.count_x):
-                        for jj in range(datinput.count_y):
-                            if datinput.data[ii][jj][1] == datinput.data[i + 1][j + 1][1]:
-                                datinput.data[ii][jj][1] = label
+                    for ii in range(norminput.count_x):
+                        for jj in range(norminput.count_y):
+                            if norminput.data[ii][jj][1] == norminput.data[i + 1][j + 1][1]:
+                                norminput.data[ii][jj][1] = label
 '''
