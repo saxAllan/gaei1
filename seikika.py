4cbd157f3a9f3441f73a5b975a7f8ba7fa4b03e2
saxAllan <asahi_shimizu@outlook.com>
2019-12-08 23:04:10 +0900

    update

--- seikika.py
+++ seikika.py
@@ -1,4 +1,5 @@
 filename_i = input("入力ファイル名（拡張子は不要）：")
+filename_o = input("入力ファイル名（拡張子は不要）：")
 
 print(filename_i + ".datを読み込んでいます...  ")
 f = open(filename_i + ".dat", "r")
@@ -10,67 +11,118 @@ f.close()
 size_org = len(orgdata)
 print("読込完了 (", size_org, "行)")
 
-
-num=len(orgdata)
-
-cnt1=[]
-cnt2=[]
-del1=[]
-del2=[]
-a=0
-b=0
-cnt1.append(1)
-cnt2.append(1)
-for i in range(1,len(orgdata)):
-    print("\r",i,end="  ")
-    if orgdata[i][1]!=orgdata[i-1][1]:
-        cnt1.append(1)
-        a+=1
-    else:
-        cnt1[a]+=1
 from operator import itemgetter
-orgdata=sorted(orgdata,key=itemgetter(0))
-for i in range(1,len(orgdata)):
-    print("\r",i,end="  ")
-    if orgdata[i][0]!=orgdata[i-1][0]:
-        cnt2.append(1)
-        b+=1
+b=sorted(orgdata,key=itemgetter(0))
+
+bottom=orgdata[0][1]
+top=orgdata[size_org-1][1]
+left=b[0][0]
+right=b[len(b)-1][0]
+delete=[]
+
+while(1):    
+    for i in range(len(orgdata)):
+        if orgdata[i][0]==left or orgdata[i][0]==right or orgdata[i][1]==bottom or orgdata[i][1]==top:
+            delete.append(i)
+    for i in range(len(delete)):
+        orgdata.pop(delete[i]-i)
+   
+    #座標カウント
+    y = orgdata[0][1]
+    i = 0
+    j = 0
+    count_x = 0
+    count_y = 0
+    while orgdata[i][1] == y:
+        count_x += 1
+        i += 1
+    count_y = len(orgdata) // count_x
+    #data = [[0]*count_y] * count_x
+    #data = [[]] * count_x
+    data = []
+    for i in range(count_x):
+        data.append([])
+
+    size_org=len(orgdata)
+
+    #正規化チェック
+    print("x=",count_x,"y=",count_y)
+    if count_x * count_y == size_org:
+        print("正規化完了")    
+
+        out=str("")
+        for i in range(size_org):
+            print("\r",i,end="  ")
+            out += str(orgdata[i][0])+" "
+            out += str(orgdata[i][1])+" "
+            out += str(orgdata[i][2])+" "
+            #out += str(orgdata[i][3])+" "
+            out += str("\n")
+
+        f=open(filename_o+".dat",mode="w")
+        f.write(out)
+        f.close()
+        print(filename_o+".dat を出力しました")
+        break
     else:
-        cnt2[b]+=1
-        
-for i in range(len(cnt1)):
-    if cnt1[i]!=max(cnt1):
-        del1.append(cnt1[i])
-for i in range(len(cnt2)):
-    if cnt2[i]!=max(cnt2):
-        del2.append(cnt2[i])
+        print("続行")
+        from operator import itemgetter
+        b=sorted(orgdata,key=itemgetter(0))
 
-for i in range(len(del1)):
-    for j in range(num):
-        print("\r",i,end="  ")
-        if orgdata[j][1]==del1[i]:
-            orgdata.pop(j)
+        bottom=orgdata[0][1]
+        top=orgdata[len(orgdata)-1][1]
+        left=b[0][0]
+        right=b[len(b)-1][0]
+        delete=[]
+    
 
-num=len(orgdata)
 
-for i in range(len(del2)):
-    for j in range(num):
-        print("\r",i,end="  ")
-        if orgdata[j][0]==del2[i]:
-            orgdata.pop(j)
+"""
+    #座標カウント
+    y = orgdata[0][1]
+    i = 0
+    j = 0
+    count_x = 0
+    count_y = 0
+    while orgdata[i][1] == y:
+        count_x += 1
+        i += 1
+    count_y = size_org // count_x
+    #data = [[0]*count_y] * count_x
+    #data = [[]] * count_x
+    data = []
+    for i in range(count_x):
+        data.append([])
 
-out=str("")
-for i in range(len(orgdata)):
-    print("\r",i,end="  ")
-    if orgdata[i][2]>b[i][2]:
-            orgdata[i][2]=b[i][2]
-    out += str(orgdata[i][0])+" "
-    out += str(orgdata[i][1])+" "
-    out += str(orgdata[i][2])+" "
-    #out += str(orgdata[i][3])+" "
-    out += str("\n")
+    #正規化チェック
+    print("x=",count_x,"y=",count_y)
+    if count_x * count_y == size_org:
+        print("正規化完了")
+        out=str("")
+        for i in range(len(orgdata)):
+            print("\r",i,end="  ")
+            out += str(orgdata[i][0])+" "
+            out += str(orgdata[i][1])+" "
+            out += str(orgdata[i][2])+" "
+            #out += str(orgdata[i][3])+" "
+            out += str("\n")
 
-f=open(filename_o+".dat",mode="w")
-f.write(out)
-f.close()
-print(filename_o+".dat を出力しました")
+        f=open(filename_o+".dat",mode="w")
+        f.write(out)
+        f.close()
+        print(filename_o+".dat を出力しました")
+        break
+    else:
+        print("続行")
+        from operator import itemgetter
+        b=sorted(orgdata,key=itemgetter(0))
+
+        bottom=orgdata[0][1]
+        top=orgdata[len(orgdata)-1][1]
+        left=b[0][0]
+        right=b[len(b)-1][0]
+        delete=[]
+"""
+
+
+
